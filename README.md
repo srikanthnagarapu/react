adaba09dda776f78c6b31e79f35ed57ec8788b14
Srikanth Nagarapu <srikanthnagarapu@Srikanths-MacBook-Air.local>
2025-04-21 15:42:25 +0530

    learned bundling and how react faster

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-#Hello Welcom to React !!!
+# Hello Welcom to React !!!
+
+# Parcel
+- Dev Build
+- Local server
+- HMR -  Hot Module Replacement
+- File Watching Algorithm - written in c++
+- Caching - Faster Builds
+- Image Optimization
+- Minification 
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Spliting 
+- Differential Bundling - 
+- Error Handling
+- HTTPs
+- Tree Shaking - remove unused code
--- app.js
+++ app.js
@@ -1,26 +1,23 @@
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement(
     "div",
     {id: "parent"},
     React.createElement(
         "div",
-        {id : "child"},
-        [
-            React.createElement("h1",{},"I'm a h1 tag"),
-            React.createElement("h1",{},"I'm a h2 tag")
-        ]
-    )
+        {id: "child"},
+        React.createElement("h1", {}, "Hello World")
+    ),
+    React.createElement(
+        "div",
+        {id: "child"},
+        React.createElement("h1", {}, "Hello World")
+    ),
 );
 
 console.log(parent)
 
-const heading = React.createElement(
-    "h1",
-    {id: "heading", xyz: "abc"},
-    "Hello World from React !!!"
-);
-console.log(heading)
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(parent);
